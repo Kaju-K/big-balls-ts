cbda5f4b3e62aa7ef76b37228591e86b5f6a81fe
Kogut <marcelokogut3@gmail.com>
2024-09-23 13:50:52 +0300

    rearranging files

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -21,7 +21,7 @@ const start = async () => {
   app.register(fastifyAutoload, { dir: join(__dirname, "routes"), options: { prefix: "/api" } });
 
   try {
-    await app.listen({ port: 3000, host: "0.0.0.0" });
+    await app.listen({ port: 4000, host: "0.0.0.0" });
   } catch (err) {
     app.log.error(err);
     process.exit(1);
